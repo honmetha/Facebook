1136266e4164157e854b075b1a417634d905ad79
Hon <dennis.mejen@gmail.com>
2019-12-21 14:32:07 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
