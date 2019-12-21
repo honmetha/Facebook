afb948702cafe179e01783a16f9dad1b7401fe01
Hon <dennis.mejen@gmail.com>
2019-12-21 15:40:34 +0700

    pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import { Route, Switch } from 'react-router-dom'
+import { Redirect } from 'react-router'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Profile from './pages/Profile'
+import Friend from './pages/Friend'
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/feed" component={Feed} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/friend" component={Friend} />
+          <Redirect to="/" />
+        </Switch>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
