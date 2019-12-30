98401f22116b2b765df7d8b8b1033066c5b84697
Hon <dennis.mejen@gmail.com>
2019-12-30 15:59:02 +0700

    Update leftNavBar

--- src/components/Leftnavbar.js
+++ src/components/Leftnavbar.js
@@ -65,6 +65,72 @@ export default class Leftnavbar extends Component {
             </Col>
           </a>
         </Row>
+        <Row className="marginBottom-3">
+          <span className="leftHeaderFont">Shortcuts</span>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="ShortcutsGroup.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Code Camp 4</span>
+            </Col>
+          </a>
+        </Row>
+        <Row className="marginBottom-3">
+          <span className="leftHeaderFont">Explore</span>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="Groups.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Groups</span>
+            </Col>
+          </a>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="Pages.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Pages</span>
+            </Col>
+          </a>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="Events.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Events</span>
+            </Col>
+          </a>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="Fundraisers.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Fundraisers</span>
+            </Col>
+          </a>
+        </Row>
+        <Row className="marginBottom-3">
+          <a href="http://localhost:3000/feed">
+            <Col span={4}>
+              <img src="AdsManager.png" alt="" width="25px" className="marginTop-10" />
+            </Col>
+            <Col span={20}>
+              <span className="blackColor">Ads Manager</span>
+            </Col>
+          </a>
+        </Row>
       </Card>
     )
   }
